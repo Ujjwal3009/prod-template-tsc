de2969758a4e75c11c618190674e092bfac72a14
Ujjwal Kumar <50870185+Ujjwal3009@users.noreply.github.com>
2024-12-23 17:29:42 +0530

    feat: Commitlint added

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends :['@commitlint/config-conventional' , '@commitlint/cli'] ,
+    rules : {
+        "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "revert" , "ci" , "build"]],
+        "subject-case": [2, "always", ["sentence-case"]],
+    }
+
+}
